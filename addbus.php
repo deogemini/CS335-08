c5bc76115d77f7b296a66392905356676fa3e768
simon <simonbrian0098@gmail.com>
2021-02-19 14:21:16 +0300

    regions update

--- addbus.php
+++ addbus.php
@@ -42,6 +42,36 @@
 
   <select name="start-from" id="" >
        <option value="dar es salaam">Dar es salaam</option> 
+       <option value="mbeya">Mbeya</option> 
+       <option value="iringa">Iringa</option> 
+       <option value="mbeya">Mbeya</option> 
+       <option value="songwe">Songwe</option> 
+       <option value="katavi">Katavi</option> 
+       <option value="kagera">Kagera</option> 
+       <option value="geita">Geita</option> 
+       <option value="kigoma">Kigoma</option> 
+       <option value="mwanza">Mwanza</option> 
+       <option value="shinyanga">Shinyanga</option> 
+       <option value="mara">Mara</option> 
+       <option value="manyara">Manyara</option> 
+       <option value="arusha">arusha</option> 
+       <option value="kilimanjaro">Kilimanjaro</option> 
+       <option value="tanga">Tanga</option> 
+       <option value="morogoro">Morogoro</option> 
+       <option value="tabora">Tabora</option> 
+       <option value="singida">Singida</option> 
+       <option value="ruvuma">Ruvuma</option> 
+       <option value="lindi">Lindi</option> 
+       <option value="mtwara">Mtwara</option> 
+       <option value="Dodoma">Dodoma</option>
+       <option value="Rukwa">Rukwa</option> 
+</select>
+  <br>
+
+  <label for="to">To:</label><br>
+  
+  <select name="destinates" id="" >
+       <option value="dodoma">dodoma</option> 
        <option value="mbeya">mbeya</option> 
        <option value="iringa">iringa</option> 
        <option value="mbeya">mbeya</option> 
@@ -63,38 +93,8 @@
        <option value="ruvuma">ruvuma</option> 
        <option value="lindi">lindi</option> 
        <option value="mtwara">mtwara</option> 
-       <option value="Dodoma">dodoma</option>
+       <option value="Dar es salaam">dar es salaam</option>
        <option value="Rukwa">rukwa</option> 
-</select>
-  <br>
-
-  <label for="to">To:</label><br>
-  
-  <select name="destinates" id="" >
-       <option value="dodoma">Dodoma</option> 
-       <option value="mbeya">Mbeya</option> 
-       <option value="iringa">Iringa</option> 
-       <option value="mbeya">Mbeya</option> 
-       <option value="songwe">Songwe</option> 
-       <option value="katavi">Katavi</option> 
-       <option value="kagera">Kagera</option> 
-       <option value="geita">Geita</option> 
-       <option value="kigoma">Kigoma</option> 
-       <option value="mwanza">Mwanza</option> 
-       <option value="shinyanga">Shinyanga</option> 
-       <option value="mara">Mara</option> 
-       <option value="manyara">Manyara</option> 
-       <option value="arusha">Arusha</option> 
-       <option value="kilimanjaro">Kilimanjaro</option> 
-       <option value="tanga">Tanga</option> 
-       <option value="morogoro">Morogoro</option> 
-       <option value="tabora">Tabora</option> 
-       <option value="singida">Singida</option> 
-       <option value="ruvuma">Ruvuma</option> 
-       <option value="lindi">Lindi</option> 
-       <option value="mtwara">Mtwara</option> 
-       <option value="Dar es salaam">Dar es salaam</option>
-       <option value="Rukwa">Rukwa</option> 
 </select>
 <br>
 <br>
